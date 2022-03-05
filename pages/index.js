d1b4a57f54c3ab79b07232a3260998893dfc381b
Mehrdad <mehrdadshirvan@gmail.com>
2022-03-05 20:44:18 +0330

    connect to api get posts

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,32 @@ import 'bootstrap/dist/css/bootstrap-grid.rtl.min.css'
 import 'bootstrap/dist/css/bootstrap.rtl.min.css'
 import Footer from "./components/footer";
 
-export default function Home() {
-  return (
-    <div id={`app`}>
-      <Head>
-        <title>Home</title>
-        <meta name="description" content="home" />
-      </Head>
-      <Header />
+function Home(props) {
 
-      <Footer />
-    </div>
-  )
+    const {posts} = props;
+    console.log(posts)
+
+    return (
+        <div id={`app`}>
+            <Head>
+                <title>Home</title>
+                <meta name="description"
+                      content="home"/>
+            </Head>
+            <Header/>
+            {posts.map((post) => (<p>{post.title}</p>))}
+            <Footer/>
+        </div>
+    )
 }
+
+
+export async function getServerSideProps(context) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const posts = await res.json()
+    return {
+        props: {posts}, // will be passed to the page component as props
+    }
+}
+
+export default Home;
